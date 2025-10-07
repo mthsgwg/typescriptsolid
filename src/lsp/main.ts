ed07edd251911f58e5c807af357a20f99f870392
Matheus Faria <matheusbahia10@gmail.com>
2025-10-07 11:45:50 -0300

    lsp

---
+++ src/lsp/main.ts
@@ -0,0 +1,34 @@
+/*
+  Liskov substitution Principle (Princípio da Substituição de Liskov)
+  Subtipos devem ser substituíveis por seus tipos de base.
+*/
+
+import {
+  FiftyPercentDiscount,
+  NoDiscount,
+  TenPercentDiscount,
+} from './classes/discount';
+import { Messaging } from './services/messaging';
+import { Order } from './classes/order';
+import { Persistency } from './services/persistency';
+import { Product } from './classes/product';
+import { ShoppingCart } from './classes/shopping-cart';
+
+const fiftyPercentDiscount = new FiftyPercentDiscount();
+// const tenPercentDiscount = new TenPercentDiscount();
+// const noDiscount = new NoDiscount();
+const shoppingCard = new ShoppingCart(fiftyPercentDiscount);
+const messaging = new Messaging();
+const persistency = new Persistency();
+const order = new Order(shoppingCard, messaging, persistency);
+
+shoppingCard.addItem(new Product('Camisa', 49.9));
+shoppingCard.addItem(new Product('Caderno', 9.9));
+shoppingCard.addItem(new Product('Lápis', 1.59));
+console.log(shoppingCard.items);
+shoppingCard.removeItem(1);
+console.log(shoppingCard.items);
+
+console.log(shoppingCard.total());
+console.log(shoppingCard.totalWithDiscount());
+order.checkout();
